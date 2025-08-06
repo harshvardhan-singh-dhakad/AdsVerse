c0f43a2a2fb5cac8a0a53883691d72a72e98975b
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-06 18:42:38 +0000

    Floating button ke andar jo bhi social media app button aur call button

--- src/components/layout/floating-action-button.tsx
+++ src/components/layout/floating-action-button.tsx
@@ -4,27 +4,41 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { MessageSquare, X, Instagram, Facebook, Linkedin, Twitter, Phone } from "lucide-react";
+import { MessageSquare, X, Phone } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const socialLinks = [
-  { href: "https://instagram.com", icon: <Instagram className="h-6 w-6" />, label: "Instagram" },
-  { href: "https://facebook.com", icon: <Facebook className="h-6 w-6" />, label: "Facebook" },
+  { href: "https://instagram.com", icon: (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+      <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
+    </svg>
+  ), label: "Instagram" },
+  { href: "https://facebook.com", icon: (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33V21.878A10.003 10.003 0 0 0 22 12z"></path>
+    </svg>
+  ), label: "Facebook" },
   { href: "https://wa.me/1234567890", icon: (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
         <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.447-4.435-9.884-9.888-9.884-5.448 0-9.886 4.434-9.889 9.884-.001 2.225.651 4.315 1.847 6.061l-1.219 4.436 4.542-1.195z" />
     </svg>
     ), label: "WhatsApp" },
-  { href: "https://linkedin.com", icon: <Linkedin className="h-6 w-6" />, label: "LinkedIn" },
-  { href: "https://twitter.com", icon: <Twitter className="h-6 w-6" />, label: "Twitter" },
+  { href: "https://linkedin.com", icon: (
+     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
+    </svg>
+  ), label: "LinkedIn" },
+  { href: "https://twitter.com", icon: (
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616v.064c0 2.296 1.634 4.208 3.791 4.649-.69.188-1.423.235-2.15.084.636 1.954 2.425 3.299 4.6 3.337-1.798 1.407-4.069 2.26-6.543 2.26-.425 0-.845-.024-1.258-.074 2.324 1.493 5.078 2.368 8.04 2.368 9.877 0 15.223-8.213 15.068-15.357.925-.668 1.72-1.503 2.338-2.439z"></path>
+    </svg>
+  ), label: "Twitter" },
   { href: "tel:+1234567890", icon: <Phone className="h-6 w-6" />, label: "Call Us" },
   { href: "https://docs.google.com/forms", icon: (
-     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-        <polyline points="14 2 14 8 20 8"></polyline>
-        <line x1="16" y1="13" x2="8" y2="13"></line>
-        <line x1="16" y1="17" x2="8" y2="17"></line>
-        <polyline points="10 9 9 9 8 9"></polyline>
+     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 11h-4v4h-2v-4H8v-2h4V7h2v4h4v2zm-8-9v-.57c0-.81.69-1.43 1.5-1.43h5c.81 0 1.5.62 1.5 1.43V4h-8zm12-2.26V18c0 1.1-.9 2-2 2H8c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h.26C6.98 2.37 6.63 3.13 6.63 4v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-.87-.35-1.63-.92-2.26z"></path>
     </svg>
   ), label: "Google Form" },
 ];
