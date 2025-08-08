81c8a9d3b7d6f5c70ba979f44b3a6fb81f51f53f
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-08 06:35:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/layout/floating-action-button.tsx
+++ src/components/layout/floating-action-button.tsx
@@ -47,7 +47,7 @@ export function FloatingActionButton() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-6 right-5 z-50">
       <div className="relative flex flex-col items-center gap-2">
         {socialLinks.map((link, index) => (
           <div
