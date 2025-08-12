3eb0ad2860bdb6b8096e1fb2fd2768b3bdeb719f
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-12 07:03:25 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,17 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "synergyflow-digital-p7c0g",
+  "appId": "1:867205490601:web:a4b9a8f0cd5c93f79346b8",
+  "storageBucket": "synergyflow-digital-p7c0g.firebasestorage.app",
+  "apiKey": "AIzaSyAKBFrXeU5o0o5tMaqeN-wAlF-KwVKtFJQ",
+  "authDomain": "synergyflow-digital-p7c0g.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "867205490601"
 };
 
 // Initialize Firebase
