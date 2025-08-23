91720560e02dcfa72c21a0e3d66ea8b5b332b564
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-23 17:17:10 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-acc

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 const teamMembers = [
   { name: "Manisha kumawat", role: "SMM & Ads Expert", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/MANU.jpg?raw=true", hint: "woman portrait" },
-  { name: "Harshvardhan Singh Dhakad", role: "Automation Specialist", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/HSD.jpeg?raw=true", hint: "man portrait" },
+  { name: "Harshvardhan Singh Dhakad", role: "Ai Automation , GEO & SEO Specialist", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/HSD.jpeg?raw=true", hint: "man portrait" },
   { name: "Aakash Dhakad", role: "Developer", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/AD.jpeg?raw=true", hint: "man portrait" },
   { name: "Shailee Jain", role: "Creative Director", avatar: "https://raw.githubusercontent.com/harshvardhan-singh-dhakad/image/895092166b5b4a22da81d9e67bc1b70570c0e930/SHAILEE.jpeg", hint: "woman smiling" },
 ];
