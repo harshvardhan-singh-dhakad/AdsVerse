f166ebc950a67e810c71b679e265af2fbb9e6f1d
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-25 10:58:40 +0000

    Paste this code as high in the <head> of the page as possible - <!-- Goo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { FloatingActionButton } from "@/components/layout/floating-action-button";
+import Script from "next/script";
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -33,11 +34,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M6GV59XL');`}
+        </Script>
+      </head>
       <body className={cn(
         "min-h-screen bg-background font-body antialiased",
         inter.variable, 
         playfairDisplay.variable
       )}>
+          <noscript>
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M6GV59XL"
+              height="0" 
+              width="0" 
+              style={{display: 'none', visibility: 'hidden'}}
+            ></iframe>
+          </noscript>
           <div className="fixed top-0 left-0 w-full h-full -z-10">
             <div className="glow-effect"></div>
             <div className="stars stars-sm"></div>
