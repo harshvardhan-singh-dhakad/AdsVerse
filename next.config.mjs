7c17e882d4453805a6472eb0500ef04aa85663a0
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-26 12:44:18 +0000

    www. redirection kaise karna hai is website me

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,41 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-       {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com'
+            }
+        ],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.adsverse.in',
+                    },
+                ],
+                destination: 'https://adsverse.in/:path*',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
