648cfef3fc64962db1c8b93345186901b2d82166
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-08-27 10:23:31 +0000

    I'm sorry, but I'm having trouble connecting to my brain right now. Plea

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { genkit, type Plugin } from "genkit";
 import { googleAI } from "@genkit-ai/googleai";
+import { firebase } from "@genkit-ai/firebase";
 
-const plugins: Plugin<any>[] = [googleAI()];
+const plugins: Plugin<any>[] = [firebase(), googleAI()];
 
 export const ai = genkit({
   plugins,
