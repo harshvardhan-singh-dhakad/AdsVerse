4bd4c1f17075f4d64f692a4326b80975f51f824a
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2025-10-31 13:03:58 +0000

    Aarsh shrivas and job titile is Business analyst pleaseupdatein meet the

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,7 +16,7 @@ const teamMembers = [
   { name: "Harshvardhan Singh Dhakad", role: "Ai Automation , GEO & SEO Specialist", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/HSD01.jpeg?raw=true", hint: "man portrait" },
   { name: "Aakash Dhakad", role: "Digital Marketing Executive", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/akash02.jpeg?raw=true", hint: "man portrait" },
   { name: "Manisha kumawat", role: "SMM & Ads Expert", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/MANU.jpg?raw=true", hint: "woman portrait" },
-  { name: "Aarsh Singh", role: "Business Analytics", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/Aarsh.JPG?raw=true", hint: "man portrait" },
+  { name: "Aarsh Shrivas", role: "Business Analyst", avatar: "https://github.com/harshvardhan-singh-dhakad/image/blob/main/Aarsh.JPG?raw=true", hint: "man portrait" },
 ];
 
 const timelineEvents = [
