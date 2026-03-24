418eef6ace4acc02387f068383c8ce26df3d78d2
Harshvardhan singh Dhakad <deepakdhakad5421@gmail.com>
2026-03-24 09:04:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "synergyflow-digital-p7c0g",
+  "appId": "1:867205490601:web:a4b9a8f0cd5c93f79346b8",
+  "apiKey": "AIzaSyAO1xYAhh9ONCZjZI2zliUn_dEPdfaZr2Y",
+  "authDomain": "synergyflow-digital-p7c0g.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "867205490601"
+};
