6d073fba57c03f8d9a548e5e58830d1c466ccb73
Harshwardhan Singh Dhakad <deepakdhakad5421@gmail.com>
2026-09-21 22:48:51 +0530

    fix: sync header with latest CMS blog posts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -373,7 +373,7 @@ export default async function RootLayout({
         >
           <div className="relative z-10 min-h-screen flex flex-col">
             <BackgroundEffects />
-            <Header navLinks={navLinks} latestPosts={[]} />
+            <Header navLinks={navLinks} latestPosts={await getLatestPosts()} />
             <main id="main-content" className="flex-1 focus:outline-none" tabIndex={-1}>
               {children}
             </main>
